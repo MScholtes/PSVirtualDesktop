10e95db312fe23548fff25d2ce7457a5e75b7ab6
MScholtes <markusscholtes@hotmail.com>
2023-08-27 17:37:29 +0200

    Update to V1.5.4

--- Module/VirtualDesktop.psm1
+++ Module/VirtualDesktop.psm1
@@ -2,8 +2,8 @@
 .SYNOPSIS
 VirtualDesktop is a module that provides commandlets to manage virtual desktops of Windows 10 and 11.
 .NOTES
-Version: 1.5.3
-Date: 2023-06-11
+Version: 1.5.4
+Date: 2023-08-27
 Author: Markus Scholtes
 #>
 
